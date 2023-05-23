a7d172934cb586c9a171163c4f997fe57fbcf3e0
Султан <carameleclipsimus@gmail.com>
2023-05-23 18:08:53 +0600

    first task of dom is done

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+Зная структуру html, с помощью изученных методов получить (в консоль):
+
+1. head
+
+2. body
+
+3. все дочерние элементы body и вывести их в консоль.
+
+4. первый div и все его дочерние узлы
+
+а) вывести все дочерние узлы в консоль
+
+б) вывести в консоль все дочерние узлы, кроме первого и последнего
+
+Для навигации по DOM использовать методы, которые возвращают только элементы
+
+*/
+
+"use strict";
+// first
+// const head = document.querySelector("head");
+// console.log(head);
+// const body = document.querySelector("body");
+// console.log(body);
+// const div = document.querySelector("div");
+// // console.log(body.childNodes);
+// // const bodyChild = body.firstElementChild.children;
+
+// const bodyFilter = [...div.children].filter(
+//   (p) => p !== div.firstElementChild && p !== div.lastElementChild
+// );
+// console.log(bodyFilter);
+
+
