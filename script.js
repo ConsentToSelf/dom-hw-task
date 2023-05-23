710c9b9de05841f4b6f6e1bd2682eecc9c8c533b
Султан <carameleclipsimus@gmail.com>
2023-05-23 19:36:24 +0600

    feat: second task of the Dom is Done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// first
 /*
 Зная структуру html, с помощью изученных методов получить (в консоль):
 
@@ -18,7 +19,6 @@
 */
 
 "use strict";
-// first
 // const head = document.querySelector("head");
 // console.log(head);
 // const body = document.querySelector("body");
@@ -32,4 +32,43 @@
 // );
 // console.log(bodyFilter);
 
+// second
+/*
+Создать функцию, которая принимает два элемента. Функция проверяет, является ли первый элемент родителем для второго:
+
+
+isParent(parent, child);
+
+isParent(document.body.children[0], document.querySelector('mark'));
+
+true так как первый див является родительским элементом для mark
+
+
+isParent(document.querySelector('ul'), document.querySelector('mark'));
+
+false так ul НЕ является родительским элементом для mark
+
+Функция принимает только DOM объекты. Обязательно проверяйте это.
+*/
+
+// const isParent = (parent, child) => {
+//   if (!(parent instanceof HTMLElement) || !(child instanceof HTMLElement)) {
+//     return "Это не Html Element";
+//   }
+//   if (parent === child.parentElement.parentElement.parentElement) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// const result = isParent(
+//   document.body.children[0],
+//   document.querySelector("mark")
+// );
+// const result2 = isParent(
+//   document.querySelector("ul"),
+//   document.querySelector("mark")
+// );
+// console.log(result);
 
